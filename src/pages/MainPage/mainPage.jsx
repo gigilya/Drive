8353d6f0a77a414d2675c3fb888107a0c1d6ea28
viktoriaa444 <118815929+viktoriaa444@users.noreply.github.com>
2024-06-11 22:16:05 +0300

    Update mainPage.jsx

---
+++ src/pages/MainPage/mainPage.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './mainPage.css';
+
+function Mainpage() {
+    return (
+      <div className="Mainpage">
+        <button id='ovalsing'>
+          <body>Sing in</body>
+        </button> 
+
+        <div id='recbase'>
+          <h1>
+            KPTC-Drive-3-1
+          </h1>
+        </div>  
+        
+        <h2 >
+          Над проектом работали
+        </h2>
+
+        <div id='recforpeople1_1'>
+          <label>
+            gigilya
+          </label>
+        </div> 
+        <div id='circle1_1'/>
+
+        <div id='recforpeople1_2'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle1_2'/>
+
+        <div id='recforpeople1_3'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle1_3'/>
+
+        <div id='recforpeople2_1'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle2_1'/>
+
+        <div id='recforpeople2_2'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle2_2'/>
+
+        <div id='recforpeople2_3'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle2_3'/>
+
+        <div id='recforpeople3_1'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle3_1'/>
+
+        <div id='recforpeople3_2'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle3_2'/>
+
+        <div id='recforpeople3_3'>
+          <label>
+          gigilya
+          </label>
+        </div> 
+        <div id='circle3_3'/>
+
+
+      </div>
+    );
+  }
+  
+  export default Mainpage;
+   
