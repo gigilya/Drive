eef9daee5edb0363653377ceb59da9b80b3690d9
tardiss <118815929+viktoriaa444@users.noreply.github.com>
2024-06-14 19:39:18 +0300

    Добавление карточки автора

---
+++ src/components/buttons/RectangleWithHuman.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './RectangleWithHuman.css';
+
+function RectangleWithHuman() {
+    return (
+        <div class = 'base'> 
+            <div class='recforpeople'/>
+            <div class='circle'/>
+        </div>
+    );
+  }
+  
+export default RectangleWithHuman;
