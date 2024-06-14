8c88f1ea1a701f884fe483ea0ac581af9d3c699d
tardiss <118815929+viktoriaa444@users.noreply.github.com>
2024-06-14 19:41:18 +0300

    Редактирование mainPage.jsx

--- src/pages/MainPage/mainPage.jsx
+++ src/pages/MainPage/mainPage.jsx
@@ -1,90 +1,45 @@
 import React from 'react';
 import './mainPage.css';
+import ButSignIn from '../../components/buttons/ButSignIn';
+import RectangleWithHuman from '../../components/buttons/RectangleWithHuman';
 
 function Mainpage() {
-    return (
-      <div className="Mainpage">
-        <button id='ovalsing'>
-          <body>Sing in</body>
-        </button> 
+  return (
+    <div className="Mainpage">
+      <body>
+        <ButSignIn />
 
-        <div id='recbase'>
+        <div class='recbase'>
           <h1>
             KPTC-Drive-3-1
           </h1>
-        </div>  
-        
+        </div>
+
         <h2 >
           Над проектом работали
         </h2>
 
-        <div id='recforpeople1_1'>
-          <label>
-            gigilya
-          </label>
-        </div> 
-        <div id='circle1_1'/>
 
-        <div id='recforpeople1_2'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle1_2'/>
+        <div id="content">
+          <div>
+            <div><RectangleWithHuman /> <label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /> <label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /> <label class = 'label'>gigilya</label></div>
+          </div>
+          <div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+          </div>
+          <div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+            <div><RectangleWithHuman /><label class = 'label'>gigilya</label></div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
 
-        <div id='recforpeople1_3'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle1_3'/>
-
-        <div id='recforpeople2_1'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle2_1'/>
-
-        <div id='recforpeople2_2'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle2_2'/>
-
-        <div id='recforpeople2_3'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle2_3'/>
-
-        <div id='recforpeople3_1'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle3_1'/>
-
-        <div id='recforpeople3_2'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle3_2'/>
-
-        <div id='recforpeople3_3'>
-          <label>
-          gigilya
-          </label>
-        </div> 
-        <div id='circle3_3'/>
-
-
-      </div>
-    );
-  }
-  
-  export default Mainpage;
-   
+export default Mainpage;
